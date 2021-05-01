ff2fff20559c4b9285718125343765832e866738
Jim Vomero <jim.vomero@fourkitchens.com>
2021-04-30 23:30:04 -0400

    Builds out app dashboard.

--- web/modules/custom/app_dashboard/src/Controller/AppDashboardController.php
+++ web/modules/custom/app_dashboard/src/Controller/AppDashboardController.php
@@ -27,15 +27,44 @@ class AppDashboardController extends ControllerBase {
   }
 
   /**
-   * Contents.
+   * Get an array all nodes by their workflow state.
+   */
+  public function getWorkflowCounts() {
+    // Quick and dirty static query.
+    $database = \Drupal::database();
+    $query = $database->query("
+      SELECT content_moderation_state.moderation_state AS state, count(*) as count
+      FROM node_field_data node_field_data
+      LEFT JOIN content_moderation_state_field_revision content_moderation_state
+        ON node_field_data.vid = content_moderation_state.content_entity_revision_id
+        AND (
+          content_moderation_state.content_entity_type_id = 'node'
+          AND content_moderation_state.content_entity_id = node_field_data.nid
+        )
+      WHERE (node_field_data.type IN ('application'))
+        AND (content_moderation_state.workflow = 'application_workflow')
+      GROUP BY content_moderation_state.moderation_state
+    ");
+    $result = $query->fetchAllKeyed();
+    return $result;
+  }
+
+  /**
+   * Page Contents.
    *
-   * @return string
-   *   Return Hello string.
+   * @return array
+   *   Render array passed to app_dashboard template.
    */
   public function contents() {
+    $counts = $this->getWorkflowCounts();
     return [
-      '#type' => 'markup',
-      '#markup' => $this->t('Implement method: contents')
+      '#theme' => 'app_dashboard',
+      '#submitted' => $counts['draft'] ?? 0,
+      '#interview' => $counts['app_requested_interview'] ?? 0,
+      '#offered' => $counts['app_offered_sent'] ?? 0,
+      '#rejected' => $counts['app_rejected'] ?? 0,
+      '#prospect' => $counts['app_future_prospect'] ?? 0,
+      '#hired' => $counts['app_hired'] ?? 0,
     ];
   }
 
